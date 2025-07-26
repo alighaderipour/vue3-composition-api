3ce851943bd4b790fcb29eb1870d83d7842cd373
Shahaz <ali.ghaderipour@gmail.com>
2025-07-26 18:25:03 +0330

    custom-vmodel

--- section4/my-vue-senior-app/src/App.vue
+++ section4/my-vue-senior-app/src/App.vue
@@ -1,15 +1,26 @@
 <script setup lang="ts">
 import SkillList from "./components/SkillList.vue";
+import SkillListNew from "./components/SkillListNew.vue"
 import {ref} from "vue";
 
 const skills = ref<string[]>([])
 
+function addSkill(newSkill: string){
+    skills.value.push(newSkill)
+}
+function removeSkill(index){
+    skills.value.splice(index, 1)
+}
 </script>
 
 <template>
 
 
-<skill-list :skills="skills"/>
+<skill-list  :skills="skills" @add-skill="addSkill" @remove-skill="removeSkill"/> 
+<hr />
+    <p style="color:green" > Custom v-model </p>
+<hr />
+<skill-list-new v-model="skills"/>
 </template>
 
 <style scoped>
--- section4/my-vue-senior-app/src/components/SkillList.vue
+++ section4/my-vue-senior-app/src/components/SkillList.vue
@@ -1,19 +1,53 @@
 <script setup lang="ts">
-import { defineProps} from "vue";
+import { PropType, defineProps, ref} from "vue";
 const props = defineProps({
   skills: {
+    type: Array as PropType<string[]>,
     default: () => []
   }
 })
+const newSkill = ref('')
+const customError = ref('')
 
+
+const emit = defineEmits<{
+  (e: 'add-skill', value: string): void;
+  (e: 'remove-skill', index: number): void;
+}>();
+
+
+function handleAddSkill(){
+  if (!newSkill.value.trim()){
+    customError.value = 'please enter a skill'
+    return
+  }
+  if (props.skills.includes(newSkill.value.trim())){
+    customError.value = 'duplicate, the skill is already in the list'
+    newSkill.value = ''
+    return
+  }
+  emit('add-skill', newSkill.value.trim())
+  newSkill.value = ''
+  customError.value =''
+}
+function handleRemoveSkill(index){
+  emit('remove-skill', index)
+  newSkill.value =''
+}
 </script>
 
 <template>
+
+<input type="text" v-model="newSkill" placeholder="enter a skill"  :class="{ 'borderError': !!customError }">
+<button type="button" @click="handleAddSkill">Submit</button>
 <ul>
+ 
+  <span v-if="customError"  :class="{ error: !!customError }" >{{ customError }}</span>
   <div>
     <p v-if="props.skills.length === 0">list is empty</p>
     <li v-for="(skill, index) in props.skills" :key="index">
     {{skill}}
+    <button type="button" @click="handleRemoveSkill(index)">removeSkill</button>
     </li>
   </div>
 
@@ -21,5 +55,11 @@ const props = defineProps({
 </template>
 
 <style scoped>
+.borderError{
+  border : solid red 1px;
+}
+.error{
+  color: red
+}
 
 </style>
