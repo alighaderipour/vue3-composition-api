e64e8a63d739972960eb9e77458d36427ad0abd5
Shahaz <ali.ghaderipour@gmail.com>
2025-07-27 11:49:36 +0330

    added vue template

---
+++ vue/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+
+
+    <div class="wrapper">
+      <Hello  />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue/src/components/Hello.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Hello'
+})
+</script>
+
+<template>
+<div>
+  <p> Ali is great! :)</p>
+</div>
+</template>
+
+<style scoped>
+
+</style>
