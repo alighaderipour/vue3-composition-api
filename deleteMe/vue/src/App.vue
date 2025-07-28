b851bded14da9cc3175a28b202876ca258742bd2
Shahaz <ali.ghaderipour@gmail.com>
2025-07-28 11:49:05 +0330

    start again

--- deleteMe/vue/src/App.vue
+++ deleteMe/vue/src/App.vue
@@ -1,21 +1,5 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-import AddSkills from '@/components/AddSkills.vue'
-const skills = ref<string[]>([])
-</script>
-
 <template>
-  <add-skills v-model="skills" />
-  <div v-if="skills.length">
-    <ul>
-      <li v-for="(skill, index) in skills">{{ skill }}</li>
-    </ul>
-  </div>
-  <div v-else :class="{ 'no-skill': skills.length === 0 }">list is empty</div>
+  <div></div>
 </template>
 
-<style scoped>
-.noSkill {
-  color: red;
-}
-</style>
+<script setup lang="ts"></script>
