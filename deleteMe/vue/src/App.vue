923f6c852aeed88d10cc3bf95e03e17df3e90371
Shahaz <ali.ghaderipour@gmail.com>
2025-08-09 12:58:07 +0330

    test

--- deleteMe/vue/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <input type="text" placeholder="enter input here" />
-</template>
-
-<script setup lang="ts">
-import { ref } from 'vue'
-
-const skills = ref<string[]>([])
-</script>
--- section2/vue-project/src/composables/useSharedUserInfo.ts
+++
@@ -1,7 +0,0 @@
-import {reactive} from 'vue';
-
-const userInfo = reactive({id: 1, name:"ali", address :"live somewhere "})
-
-export function useSharedUserInfo(){
-  return {userInfo}
-}
