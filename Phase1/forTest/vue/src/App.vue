b5caafb4c46e58367e0301d57062bb5781c64921
Shahaz <ali.ghaderipour@gmail.com>
2025-08-10 23:22:31 +0330

    Test

--- Phase1/forTest/vue/src/App.vue
+++ Phase1/forTest/vue/src/App.vue
@@ -1,11 +1,9 @@
 <script setup>
 import Counter from '@/components/Counter.vue'
-import CounterTwo from '@/components/CounterTwo.vue'
 </script>
 
 <template>
   <Counter />
-  <CounterTwo />
 </template>
 
 <style scoped></style>
--- Phase1/forTest/vue/src/router/test.js
+++
@@ -1,6 +0,0 @@
-const skills = ['python', 'javascript']
-const newSkills = [...skills]
-console.log(newSkills)
-
-const sortedSkills = [...skills].sort()
-console.log(sortedSkills)
